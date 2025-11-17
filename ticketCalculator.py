ff17bcd45b27cd7176b7d8f5b6bb11fc79511086
Jonathan Gilmer <jpcgilmer@gmail.com>
2025-11-16 22:41:28 -0500

    finish ticket

--- ticketCalculator.py
+++ ticketCalculator.py
@@ -11,8 +11,14 @@ class Ticket:
             return True
         else:
             return False
+        
+    def climate_impact(self):
+        about_climate = "The impact of ticket production on climate change is significant due to resource consumption and emissions."
+        return about_climate
+    
     
 ticket = Ticket(10, "A123450110")
 total = ticket.calculate_total(5)  # Calling the method
 print(f"Total ticket price for 5 tickets: {total}"+"USD")
 print(f"Is valid ticket: {ticket.is_ticket()}")
+print(ticket.climate_impact())
